a14e09825149a6d8d2a087b5cab660173ad3fcd4
Maciej Gębarski <mgebarski@gmail.com>
2020-06-26 17:43:15 +0200

    add comments service address

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const axios = require('axios');
 
 const app = express();
-
 app.use(bodyParser.json());
 
 const events = [];
@@ -14,7 +13,7 @@ app.post('/events', (req, res) => {
   events.push(event);
 
   axios.post('http://posts-clusterip-srv:4000/events', event);
-  // axios.post('http://localhost:4001/events', event);
+  axios.post('http://comments-clusterip-srv:4001/events', event);
   // axios.post('http://localhost:4002/events', event);
   // axios.post('http://localhost:4003/events', event);
   res.send({status: 'Ok'});
